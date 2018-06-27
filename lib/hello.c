6bafb66077684358aab9e01bec7c8cfd205018e4
Ken Ballou <ballou@crab.qozzy.com>
2018-06-27 16:48:51 -0400

    Change package name

--- lib/hello.c
+++ lib/hello.c
@@ -4,13 +4,18 @@
 #include <stdio.h>
 
 #if ENABLE_NLS && HAVE_GETTEXT
-#define _(s) gettext(s)
+#define HELLOPACKAGE "hello"
+#define _(s) dgettext(HELLOPACKAGE, s)
 #else
 #define _(s) s
 #endif
 
 void greet (const char * name)
 {
+#if ENABLE_NLS && HAVE_GETTEXT
+    bindtextdomain (HELLOPACKAGE, LOCALEDIR);
+#endif
+    
     if (name != NULL)
     {
         printf (_("Hello, %s\n"), name);
